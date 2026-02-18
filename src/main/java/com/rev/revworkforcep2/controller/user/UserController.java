9c44f735c3237841ef47b4ac0d4c58851a979371
thulasikumar0423 <thulasikumar216@gmail.com>
2026-02-18 18:04:35 +0530

    Added role-based and ownership-based authorization with @PreAuthorize and custom UserAuthorization logic

--- src/main/java/com/rev/revworkforcep2/controller/user/UserController.java
+++ src/main/java/com/rev/revworkforcep2/controller/user/UserController.java
@@ -1,4 +1,34 @@
 package com.rev.revworkforcep2.controller.user;
 
+import com.rev.revworkforcep2.dto.response.user.UserResponse;
+import com.rev.revworkforcep2.util.ApiResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
 public class UserController {
+
+    /*Admin Only Example
+    @PreAuthorize("hasRole('ADMIN')")
+    @PostMapping("/create")
+    public ApiResponse<UserResponse> createUser(...) {
+
+    // Manager Only Example
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping("/team")
+    public ApiResponse<List<UserResponse>> getTeamMembers() {
+
+    // Employee Access (Self + higher roles)
+    @PreAuthorize("hasAnyRole('EMPLOYEE','MANAGER','ADMIN')")
+    @GetMapping("/profile")
+    public ApiResponse<UserResponse> getProfile() {}
+    */
+
+
 }
