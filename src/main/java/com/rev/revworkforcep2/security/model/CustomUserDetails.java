0113f61e9be92b23af7cc52c081512b97669eb03
thulasikumar0423 <thulasikumar216@gmail.com>
2026-02-18 10:31:57 +0530

    Complete Project Structure

---
+++ src/main/java/com/rev/revworkforcep2/security/model/CustomUserDetails.java
@@ -0,0 +1,4 @@
+package com.rev.revworkforcep2.security.model;
+
+public class CustomUserDetails {
+}
---
+++ src/main/java/com/rev/revworkforcep2/service/auth/impl/AuthServiceImpl.java
@@ -0,0 +1,4 @@
+package com.rev.revworkforcep2.service.auth.impl;
+
+public class AuthServiceImpl {
+}
